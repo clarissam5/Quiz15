769e3d97b8677d4ad4e045643ef08847cc12ca09
clarissam5 <clarissam037@gmail.com>
2018-10-15 16:14:54 -0400

    Added quiz 15 jQuery calc files

---
+++ calculate.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".inputElements").on("click", function () {
+        $("#calculatorInput").append($(this).text());
+    });
+    $(".clear").on("click", function () {
+        $("#calculatorInput").empty();
+    });
+    $(".evaluate").on("click", function () {
+        try {
+            var z = $("#calculatorInput").text();
+            $("#calculatorInput").text(eval(z));
+        }
+        catch (err) {
+            var message = document.getElementById("calculatorInput");
+            message.innerHTML = "Invalid calculator input.";
+        }
+
+    });
+});
